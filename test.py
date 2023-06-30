af863cf0f778e35aec8ad2d3c03f854e603a400f
Shashank1985 <32988364+Shashank1985@users.noreply.github.com>
2023-06-30 18:11:59 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,12 @@
+#deploying the symptom to disease model using streamlit
+#done by sensei0410 on 30/06/2023
+
+import streamlit as st
+import pandas as pd
+
+st.title("""WELCOME""")
+st.header("Enter your symptoms")
+
+st.text_input("enter symptoms")
+
+best_model = 
